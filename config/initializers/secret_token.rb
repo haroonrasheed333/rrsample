4cffba42fb930101c7780b85883542ab09c28fac
Haroon Rasheed <haroonrasheed@berkeley.edu>
2013-09-24 14:03:41 -0700

    Gemfile Update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,20 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '05876a1e1cb070387b145627d0e9bfaf3af93d95f1893728dcda36f3277ca3eda6f4dac1550f4226111f2a1e1db2cace77ab43312cf715cb83b981909af3d9b1'
+# FirstApp::Application.config.secret_token = '05876a1e1cb070387b145627d0e9bfaf3af93d95f1893728dcda36f3277ca3eda6f4dac1550f4226111f2a1e1db2cace77ab43312cf715cb83b981909af3d9b1'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
